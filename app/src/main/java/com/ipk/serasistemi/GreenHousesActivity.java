998daecd8324270ff0534b34d5f881beeaa7c679
ipek <cw_ipk@outlook.com>
2020-11-07 09:42:42 +0300

    Green House close/open switch updated

--- app/src/main/java/com/ipk/serasistemi/GreenHousesActivity.java
+++ app/src/main/java/com/ipk/serasistemi/GreenHousesActivity.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class GreenHousesActivity extends AppCompatActivity {
     LinearLayout lay_gh1, lay_gh2, lay_gh3;
+    Switch sw_gh1, sw_gh2, sw_gh3;
+    TextView heat_gh1, heat_gh2, heat_gh3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,55 @@ public class GreenHousesActivity extends AppCompatActivity {
         lay_gh1=findViewById(R.id.greenHouse1);
         lay_gh2=findViewById(R.id.greenHouse2);
         lay_gh3=findViewById(R.id.greenHouse3);
+
+        sw_gh1=findViewById(R.id.gh1_switch);
+        sw_gh2=findViewById(R.id.gh2_switch);
+        sw_gh3=findViewById(R.id.gh3_switch);
+
+        heat_gh1=findViewById(R.id.gh1_heat);
+        heat_gh2=findViewById(R.id.gh2_heat);
+        heat_gh3=findViewById(R.id.gh3_heat);
+
+        //bunun bilgisi nerede tutulacaksa oradan alınmalı en başta
+        sw_gh1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!sw_gh1.isChecked()){
+                    sw_gh1.setText("Açık    ");
+                    sw_gh1.setTextColor(getResources().getColor(R.color.positive));
+                }else{
+                    sw_gh1.setText("Kapalı    ");
+                    sw_gh1.setTextColor(getResources().getColor(R.color.negative));
+                }
+            }
+        });
+
+
+        sw_gh2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!sw_gh2.isChecked()){
+                    sw_gh2.setText("Açık    ");
+                    sw_gh2.setTextColor(getResources().getColor(R.color.positive));
+                }else{
+                    sw_gh2.setText("Kapalı    ");
+                    sw_gh2.setTextColor(getResources().getColor(R.color.negative));
+                }
+            }
+        });
+
+        sw_gh3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!sw_gh3.isChecked()){
+                    sw_gh3.setText("Açık    ");
+                    sw_gh3.setTextColor(getResources().getColor(R.color.positive));
+                }else{
+                    sw_gh3.setText("Kapalı    ");
+                    sw_gh3.setTextColor(getResources().getColor(R.color.negative));
+                }
+            }
+        });
 
         final Intent intent=new Intent(getApplicationContext(),DetailedGhActivity.class);
         lay_gh1.setOnClickListener(new View.OnClickListener() {
